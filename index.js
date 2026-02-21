f1dff938d74d75418135564ddcfdf419c6dc8ecc
Onix43 <piyvoda20033@gmail.com>
2026-02-21 15:48:45 +0000

    Deploying to gh-pages from @ Onix43/project-artist-hub@30712add29d6469f9cf4c2a365b3c503ddee3890 🚀

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import{S as l,N as c,P as u,a as p}from"./assets/vendor-fJp_pnmi.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const r of e.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function n(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function i(t){if(t.ep)return;t.ep=!0;const e=n(t);fetch(t.href,e)}})();new l(".swiper",{spaceBetween:20,loop:!0,speed:500,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},modules:[c,u]});const d=p.create({baseURL:"https://sound-wave.b.goit.study/api"});async function f(o){return(await d.get("/artists",{params:{limit:8,...o}})).data}const m={learnMoteBtn:document,listArtists:document.querySelector(".artists-list")},g="/project-artist-hub/assets/symbol-defs-CxF1Hgjl.svg";async function y(){const{artists:o}=await f(),s=o.map(({_id:n,strArtist:i,strBiographyEN:t,strArtistThumb:e,genres:r})=>`
-        <li class="artists-list-item" data-artist-id="${n}">
+import{S as d,N as u,P as p,a as f}from"./assets/vendor-fJp_pnmi.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const s of e.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function o(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function i(t){if(t.ep)return;t.ep=!0;const e=o(t);fetch(t.href,e)}})();new d(".swiper",{spaceBetween:20,speed:500,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",dynamicBullets:!0,type:"bullets",clickable:!0},modules:[u,p]});const c=f.create({baseURL:"https://sound-wave.b.goit.study/api"});async function m(n){return(await c.get("/artists",{params:{limit:8,...n}})).data}const l={learnMoteBtn:document,listArtists:document.querySelector(".artists-list"),listFeedbacks:document.querySelector(".js-list-feedbacks")},g="/project-artist-hub/assets/symbol-defs-CxF1Hgjl.svg";async function b(){const{artists:n}=await m(),a=n.map(({_id:o,strArtist:i,strBiographyEN:t,strArtistThumb:e,genres:s})=>`
+        <li class="artists-list-item" data-artist-id="${o}">
           <div class="list-item-img">
             <img src="${e}" alt="${i}" />
           </div>
           <ul class="artists-tags-list">
-            ${r.map(a=>`
-                <li class="tags-list-item">${a}</li>
+            ${s.map(r=>`
+                <li class="tags-list-item">${r}</li>
               `).join("")}
           </ul>
           <h3 class="list-item-title">${i}</h3>
@@ -21,5 +21,20 @@ import{S as l,N as c,P as u,a as p}from"./assets/vendor-fJp_pnmi.js";(function()
             </span>
           </button>
         </li>
-      `).join("");m.listArtists.insertAdjacentHTML("beforeend",s)}y();
+      `).join("");l.listArtists.insertAdjacentHTML("beforeend",a)}b();async function v(){return(await c.get("/feedbacks",{params:{limit:10,page:1}})).data}async function y(){const a=(await v()).data.map(({name:o,rating:i,descr:t})=>{const e=i-Math.floor(i),s=Math.floor(i);let r;return e>=.1&&e<=.2?r=s:e>.2&&e<.8?r=`${s}-5`:e>=.8&&e<=.9?r=s+1:r=s,`
+         <li class="swiper-slide">
+            <div class="wrapper">
+              <div class="stars-static rating-set-${r}">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+              </div>
+              <p class="user-feedback">
+              ${t}
+              </p>
+              <p class="user-name">${o}</p>
+            </div>
+          </li>`}).join("");l.listFeedbacks.insertAdjacentHTML("beforeend",a)}y();
 //# sourceMappingURL=index.js.map
