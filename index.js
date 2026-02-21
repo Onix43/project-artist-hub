6ca063bc87b43c9f2f59f9dbc4eae9dccb6dc87c
Onix43 <piyvoda20033@gmail.com>
2026-02-21 10:06:57 +0000

    Deploying to gh-pages from @ Onix43/project-artist-hub@15c960d79a1cc8361cb2d4ec78b61a44f50b1ddc 🚀

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import{a as c}from"./assets/vendor-BJ9gahTP.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const i of e.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function n(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function r(t){if(t.ep)return;t.ep=!0;const e=n(t);fetch(t.href,e)}})();const l=c.create({baseURL:"https://sound-wave.b.goit.study/api"});async function u(o){return(await l.get("/artists",{params:{limit:8,...o}})).data}const d={learnMoteBtn:document,listArtists:document.querySelector(".artists-list")},f="/project-artist-hub/assets/symbol-defs-CjLODlaq.svg";async function m(){const{artists:o}=await u(),s=o.map(({_id:n,strArtist:r,strBiographyEN:t,strArtistThumb:e,genres:i})=>`
+import{S as l,N as c,P as u,a as p}from"./assets/vendor-fJp_pnmi.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const r of e.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function n(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function i(t){if(t.ep)return;t.ep=!0;const e=n(t);fetch(t.href,e)}})();new l(".swiper",{spaceBetween:20,loop:!0,speed:500,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},modules:[c,u]});const d=p.create({baseURL:"https://sound-wave.b.goit.study/api"});async function f(o){return(await d.get("/artists",{params:{limit:8,...o}})).data}const m={learnMoteBtn:document,listArtists:document.querySelector(".artists-list")},g="/project-artist-hub/assets/symbol-defs-CjLODlaq.svg";async function y(){const{artists:o}=await f(),s=o.map(({_id:n,strArtist:i,strBiographyEN:t,strArtistThumb:e,genres:r})=>`
         <li class="artists-list-item" data-artist-id="${n}">
           <div class="list-item-img">
-            <img src="${e}" alt="${r}" />
+            <img src="${e}" alt="${i}" />
           </div>
           <ul class="artists-tags-list">
-            ${i.map(a=>`
+            ${r.map(a=>`
                 <li class="tags-list-item">${a}</li>
               `).join("")}
           </ul>
-          <h3 class="list-item-title">${r}</h3>
+          <h3 class="list-item-title">${i}</h3>
           <p class="list-item-descr">
             ${t}
           </p>
@@ -16,10 +16,10 @@ import{a as c}from"./assets/vendor-BJ9gahTP.js";(function(){const s=document.cre
             Load More
             <span>
               <svg width="8" height="14">
-                <use href="${f}#learn-more"></use>
+                <use href="${g}#learn-more"></use>
               </svg>
             </span>
           </button>
         </li>
-      `).join("");d.listArtists.insertAdjacentHTML("beforeend",s)}m();
+      `).join("");m.listArtists.insertAdjacentHTML("beforeend",s)}y();
 //# sourceMappingURL=index.js.map
