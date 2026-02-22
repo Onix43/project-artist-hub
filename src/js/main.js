03f813ab894f4fb0f0e2665a8969662543c067e8
Onix <piyvoda20033@gmail.com>
2026-02-22 13:08:04 +0200

    fixed sjs structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,4 @@
 import './utils/swiper';
 import './components/artists/artists';
-import { openArtistModal } from './render/render-modal';
-
-// Handle artist modal opening
-const artistsList = document.querySelector('.artists-list');
-if (artistsList) {
-  artistsList.addEventListener('click', e => {
-    const btn = e.target.closest('.list-item-btn');
-    if (!btn) return;
-
-    const artistId = btn.closest('[data-artist-id]').dataset.artistId;
-    if (artistId) {
-      openArtistModal(artistId);
-    }
-  });
-}
 import './render/render-feedbacks';
+import './components/modal/modal-listeners';
