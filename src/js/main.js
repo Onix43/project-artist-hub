4a334a69d9973fb51c4e7d6848cde13de1a735ea
hordzich ihar <Garik666770@gmail.com>
2026-02-22 17:24:52 +0300

    headr-css-js

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@ import './utils/swiper';
 import './components/artists/artists';
 import './render/render-feedbacks';
 import './components/modal/modal-listeners';
+import './components/headr/burger-close'
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,3 +2,10 @@ export default {
   listArtists: document.querySelector('.artists-list'),
   listFeedbacks: document.querySelector('.js-list-feedbacks'),
 };
+
+// headr
+export const refs = {
+  menu: document.querySelector('[data-menu]'),
+  toggleBtn: document.querySelector('[data-menu-toggle]'),
+  navLinks: document.querySelectorAll('.nav-list a'),
+};
