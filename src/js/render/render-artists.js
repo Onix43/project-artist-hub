49199d9f69fdf19dc77472fc1b67f347d402d282
JustFun <paw93@ukr.net>
2026-02-23 16:24:35 +0200

    add empty artist field
    1)создал функцию createFilterError. Которая отрисовывает разметку  при некорректном фильтре.
    2) Перенес все  стили  согласно макета
    3) Добавил импорт  функции в render-artists.js

--- src/js/render/render-artists.js
+++ src/js/render/render-artists.js
@@ -1,5 +1,6 @@
 import { getArtists } from '../api/artists-api';
 import { createArtistsList } from '../components/artists/createList';
+import { createFilterError } from '../components/artists/createFilterError';
 import { onLoadMoreClick } from '../components/artists/loadMoreCard';
 import refs from '../refs';
 
