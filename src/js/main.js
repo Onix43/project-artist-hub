53f1da8ba495b4a53a5a92a79e91fd3e2ef40dfa
Ivan <islaguta@gmail.com>
2026-02-25 21:35:32 +0100

    modal-feedback-version-2

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,4 @@ import './render/render-feedbacks';
 import './render/render-artists';
 import './components/modal/modal-listeners';
 import './components/headr/burger-close';
+import './components/modal/feedback-modal';
